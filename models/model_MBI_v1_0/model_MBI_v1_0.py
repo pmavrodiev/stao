88f8a3f38aa65447b2be4c769a569d6379cd34ea
Mavrodiev Pavlin <pavlin.mavrodiev@nagra.com>
2017-07-29 22:14:57 +0200

    implemented sweep over all possible parameter combinations

--- models/model_MBI_v1_0/model_MBI_v1_0.py
+++ models/model_MBI_v1_0/model_MBI_v1_0.py
@@ -40,38 +40,15 @@ class model_MBI_v_1_0(ModelBase):
         return 'Model_MBI_v1.0'
 
     def process_settings(self, config):
-        # first check if we are doing a parameter sweep
-        self.param_basic_sweep = False  # by default False
-        self.param_ov_sweep = False  # by default False
-        if config.has_option('parameter_basic_sweep', 'slope_lat') and \
-           config.has_option('parameter_basic_sweep', 'slope_rlat') and \
-           config.has_option('parameter_basic_sweep', 'fahrzeit_cutoff'):
-            self.slope_lat_sweep = [float(x) for x in json.loads(config.get('parameter_basic_sweep', 'slope_lat'))]
-            self.slope_rlat_sweep = [float(x) for x in json.loads(config.get('parameter_basic_sweep', 'slope_rlat'))]
-            self.fahrzeit_cutoff_sweep = [float(x) for x in json.loads(config.get('parameter_basic_sweep',
-                                                                                  'fahrzeit_cutoff'))]
-            self.param_basic_sweep = True
-        ####
-        if config.has_option('parameters_ov_sweep', 'beta_ov_sweep') and \
-           config.has_option('parameters_ov_sweep', 'f_pendler_sweep') and \
-           config.has_option('parameters_ov_sweep', 'pendler_ausgaben_sweep'):
-            self.beta_ov_sweep = [float(x) for x in json.loads(config.get('parameters_ov_sweep', 'beta_ov_sweep'))]
-            self.f_pendler_sweep = [float(x) for x in json.loads(config.get('parameters_ov_sweep', 'f_pendler_sweep'))]
-            self.pendler_ausgaben_sweep = [float(x) for x in json.loads(config.get('parameters_ov_sweep',
-                                                                                   'pendler_ausgaben_sweep'))]
-            self.param_ov_sweep = True
-        #
-        if self.param_ov_sweep and self.param_basic_sweep:
-            self.logger.error('Parameter sweep over both basic and ov parameters not yet implemented')
-            sys.exit(1)
         try:
-            self.slope_lat = float(config["parameters"]["slope_lat"])
-            self.slope_rlat = float(config["parameters"]["slope_rlat"])
-            self.fahrzeit_cutoff = float(config["parameters"]["fahrzeit_cutoff"])
-            #
-            self.beta_ov = float(config["parameters"]["beta_ov"])
-            self.f_pendler = float(config["parameters"]["f_pendler"])
-            self.pendler_ausgaben = float(config["parameters"]["pendler_ausgaben"])
+            # Parameters Haushaltausgaben
+            self.slope_lat = [float(x) for x in json.loads(config["parameters"]["slope_lat"])]
+            self.slope_rlat = [float(x) for x in json.loads(config["parameters"]["slope_rlat"])]
+            self.fahrzeit_cutoff = [float(x) for x in json.loads(config["parameters"]["fahrzeit_cutoff"])]
+            # Parameters oeV
+            self.beta_ov = [float(x) for x in json.loads(config["parameters"]["beta_ov"])]
+            self.f_pendler = [float(x) for x in json.loads(config["parameters"]["f_pendler"])]
+            self.pendler_ausgaben = [float(x) for x in json.loads(config["parameters"]["pendler_ausgaben"])]
             #
             self.umsatz_output_csv = config["output"]["output_csv"]
         except Exception:
@@ -114,50 +91,73 @@ class model_MBI_v_1_0(ModelBase):
                         self.whoami(), "['all_stores', 'sbb_stations']", list(tables_dict.keys()))
             sys.exit(1)
 
-        if self.param_basic_sweep:
-            self.analysis_sweep(pandas_dt)
-            return 0
+        # Loop over all parameters
+        for lat in self.slope_lat:
+            for rlat in self.slope_rlat:
+                for fz_cutoff in self.fahrzeit_cutoff:
 
-        pandas_postprocessed_dt = self.compute_market_share(pandas_dt, self.slope_lat, self.slope_rlat,
-                                                            self.fahrzeit_cutoff)
+                    pandas_postprocessed_dt = self.compute_market_share(pandas_dt, lat, rlat, fz_cutoff)
 
-        # --- HAUSHALT component -----------------------------------------------------------------------------------
-        # pandas_preprocessed_dt now has a column 'LMA'
-        # giving the local market share of store i
-        # in each hektar from which i can be reached
-        umsatz_potential_pd = self.gen_umsatz_prognose(pandas_postprocessed_dt)
+                    # --- HAUSHALT component -----------------------------------------------------------------------
+                    # pandas_preprocessed_dt now has a column 'LMA'
+                    # giving the local market share of store i
+                    # in each hektar from which i can be reached
+                    umsatz_potential_pd = self.gen_umsatz_prognose(pandas_postprocessed_dt)
 
-        # pendler einfluss is modelled after the Haushalt part
-        if self.param_ov_sweep:
-            self.analysis_ov_sweep(umsatz_potential_pd, stations_pd)
-            return 0
+                    for beta_ov in self.beta_ov:
+                        for f_pendler in self.f_pendler:
+                            pendler_einfluss_pd = self.calc_zusaetzliche_kauefer(pandas_postprocessed_dt,
+                                                                                 stations_pd, beta_ov, f_pendler)
+                            # left join between the calculated umsatz and the pendler einfluss
+                            umsatz_potential_pd = pd.merge(umsatz_potential_pd, pendler_einfluss_pd,
+                                                           how='left', left_index=True, right_index=True)
 
-        pendler_einfluss_pd = self.calc_zusaetzliche_kauefer(pandas_postprocessed_dt, stations_pd, self.beta_ov,
-                                                             self.f_pendler)
-        # left join between the calculated umsatz and the pendler einfluss
-        umsatz_potential_pd = pd.merge(umsatz_potential_pd, pendler_einfluss_pd, how='left', left_index=True,
+                            for pendler_ausgaben in self.pendler_ausgaben:
+                                self.logger.info('Calculating Final Umsaetze ...')
+                                self.logger.info("Parameters: pendler_ausgaben = %f", pendler_ausgaben)
+                                umsatz_potential_pd['Umsatz_Pendler'] = umsatz_potential_pd['additional_kaeufer'] * \
+                                                                        pendler_ausgaben
 
-                                       right_index=True)
+                                umsatz_potential_pd['Umsatz_Total'] = np.where(
+                                    np.isnan(umsatz_potential_pd['Umsatz_Pendler']),
+                                    umsatz_potential_pd['Umsatz_Haushalte'],
+                                    umsatz_potential_pd['Umsatz_Haushalte'] + umsatz_potential_pd['Umsatz_Pendler'])
 
-        umsatz_potential_pd['Umsatz_Pendler'] = umsatz_potential_pd['additional_kaeufer'] * self.pendler_ausgaben
-        umsatz_potential_pd['Umsatz_Total'] = np.where(np.isnan(umsatz_potential_pd['Umsatz_Pendler']),
-                                                       umsatz_potential_pd['Umsatz_Haushalte'],
-                                                       umsatz_potential_pd['Umsatz_Haushalte'] +
-                                                       umsatz_potential_pd['Umsatz_Pendler'])
-
-        umsatz_potential_pd['percentage_istU_modelU'] = (umsatz_potential_pd['Umsatz_Total'] -
+                                umsatz_potential_pd['percentage_istU_modelU'] = (umsatz_potential_pd['Umsatz_Total'] -
                                                                  umsatz_potential_pd['istUmsatz']) / \
-            umsatz_potential_pd['istUmsatz']
+                                                                 umsatz_potential_pd['istUmsatz']
 
-        # re-order only the relevant columns
-        column_order = ['StoreName', 'Retailer', 'Format', 'VFL', 'Adresse', 'PLZ', 'Ort',
-                        'HARasterID', 'E_LV03', 'N_LV03', 'ProfitKSTID', 'Food', 'Frische', 'Near/Non Food',
-                        'Fachmaerkte', 'additional_kaeufer', 'istUmsatz', 'Umsatz_Haushalte', 'Umsatz_Pendler',
-                        'Umsatz_Total']
+                                # re-order only the relevant columns
+                                column_order = ['StoreName', 'Retailer', 'Format', 'VFL', 'Adresse', 'PLZ', 'Ort',
+                                                'HARasterID', 'E_LV03', 'N_LV03', 'ProfitKSTID', 'Food', 'Frische',
+                                                'Near/Non Food', 'Fachmaerkte', 'additional_kaeufer', 'istUmsatz',
+                                                'Umsatz_Haushalte', 'Umsatz_Pendler', 'Umsatz_Total']
 
-        umsatz_potential_pd = umsatz_potential_pd[column_order]
-        self.logger.info('Exporting Umsatz predictions to csv')
-        umsatz_potential_pd.to_csv(self.umsatz_output_csv)
+                                umsatz_potential_pd = umsatz_potential_pd[column_order]
+
+                                # --- Calculate the error ------------------------------------------------
+                                error_E_i = np.power(umsatz_potential_pd['Umsatz_Total'] -
+                                                     umsatz_potential_pd['istUmsatz'], 2) / \
+                                            umsatz_potential_pd['istUmsatz']
+                                error_E_i = error_E_i.loc[~np.isnan(error_E_i)]
+
+                                total_error = np.sqrt(np.sum(error_E_i))
+                                error_quantile = error_E_i.quantile(q=0.95)
+                                total_error_0_95 = np.sqrt(np.sum(error_E_i.loc[error_E_i <= error_quantile]))
+                                self.logger.info("TOTAL ERROR: %f / %f", total_error, total_error_0_95)
+
+                                if total_error_0_95 < self.E_min[0][0]:
+                                    self.E_min = [(total_error,
+                                                   {"lat": lat, "rlat": rlat, "fz_cutoff": fz_cutoff,
+                                                    "beta_ov": beta_ov, "f_pendler": f_pendler,
+                                                    "pendler_ausgaben": pendler_ausgaben}
+                                                   )]
+                                    self.logger.info('New minimum found.')
+                                self.logger.info('Exporting Umsatz predictions to csv')
+                                output_fname = self.umsatz_output_csv + "_lat_" + str(lat) + "_rlat_" + str(rlat) + \
+                                               "_fzcutoff_" + str(fz_cutoff) + "_betaov_" + str(beta_ov) + \
+                                               "_fpendler_" + str(f_pendler) + "_pendler_ausgaben_" + pendler_ausgaben
+                                umsatz_potential_pd.to_csv(output_fname)
 
     def calc_zusaetzliche_kauefer(self, stores_pd, stations_pd, beta, f):
         def get_reachable_stations(group, radius):
@@ -216,91 +216,6 @@ class model_MBI_v_1_0(ModelBase):
         return y.reset_index().groupby('StoreID',
                                        as_index=False)['additional_kaeufer'].aggregate(np.sum).set_index('StoreID')
 
-    def analysis_ov_sweep(self, umsatz_dt, stores_pd, referenz_pd, stations_pd):
-
-        self.logger.info('Will do oV parameter sweep. This will take a while, better run over night')
-        for beta in self.beta_ov_sweep:
-            for f_pendler in self.f_pendler_sweep:
-                for pendler_ausgaben in self.pendler_ausgaben_sweep:
-                    pendler_einfluss_pd = self.calc_zusaetzliche_kauefer(stores_pd, stations_pd, beta, f_pendler)
-
-                    # left join between the calculated umsatz and the pendler einfluss
-                    umsatz_potential_pd = pd.merge(umsatz_dt, pendler_einfluss_pd,
-                                                   how='left', left_index=True, right_index=True)
-
-                    umsatz_potential_pd['umsatz_with_pendler'] = umsatz_potential_pd['Umsatz_Haushalte'] + \
-                        umsatz_potential_pd['additional_kaeufer'] * pendler_ausgaben
-
-                    umsatz_potential_pd.loc[np.isnan(umsatz_potential_pd['umsatz_with_pendler']),
-                                            'umsatz_with_pendler'] = \
-                        umsatz_potential_pd[np.isnan(umsatz_potential_pd['umsatz_with_pendler'])]['Umsatz_Haushalte']
-
-                    umsatz_potential_pd['verhaeltnis_tU_pendler_prozent'] = \
-                        (umsatz_potential_pd['umsatz_with_pendler'] -
-                         umsatz_potential_pd['Tatsechlicher Umsatz - FOOD_AND_FRISCHE']) / \
-                        umsatz_potential_pd['Tatsechlicher Umsatz - FOOD_AND_FRISCHE']
-
-                    # calculate the individual errors
-                    umsatz_potential_pd['E_i'] = np.power(umsatz_potential_pd['umsatz_with_pendler'] -
-                                                          umsatz_potential_pd['Tatsechlicher Umsatz - '
-                                                                              'FOOD_AND_FRISCHE'], 2) / \
-                        umsatz_potential_pd['Tatsechlicher Umsatz - FOOD_AND_FRISCHE']
-
-                    total_error = np.sqrt(umsatz_potential_pd.E_i.sum())
-
-                    error_quantile = umsatz_potential_pd[~np.isnan(umsatz_potential_pd['E_i'])]['E_i'].quantile(q=0.99)
-                    total_error_0_99 = np.sqrt(umsatz_potential_pd[umsatz_potential_pd['E_i'] <=
-                                                                   error_quantile]['E_i'].sum())
-                    self.logger.info("TOTAL ERROR: %f / %f", total_error, total_error_0_99)
-
-                    if total_error_0_99 < self.E_min[0][0]:
-                        self.E_min = [(total_error, {"beta": beta, "f_pendler": f_pendler,
-                                                     "pendler_ausgaben": pendler_ausgaben})]
-                        self.logger.info('New minimum found.')
-
-                    self.logger.info('Exporting Umsatz predictions to csv')
-
-                    umsatz_potential_pd.to_csv(self.umsatz_output_csv + '_beta_' + str(beta) + '_fPendler_' +
-                                               str(f_pendler) + '_pAusgaben_' + str(pendler_ausgaben))
-
-        self.logger.info('Found error minimum of %f for beta=%f / f_pendler=%f / pendler_ausgaben=%f',
-                         self.E_min[0][0], self.E_min[0][1]["beta"], self.E_min[0][1]["f_pendler"],
-                         self.E_min[0][1]["pendler_ausgaben"])
-
-    def analysis_sweep(self, pandas_dt):
-        self.logger.info('Will do basic parameter sweep. This will take a while, better run over night')
-
-        for lat in self.slope_lat_sweep:
-            for rlat in self.slope_rlat_sweep:
-                for fz in self.fahrzeit_cutoff_sweep:
-                    pandas_sweeped_dt = self.compute_market_share(pandas_dt, lat, rlat, fz)
-                    # pandas_preprocessed_dt now has a column 'local_market_share' giving the
-                    # local market share of store i in each hektar
-                    umsatz_potential_pd = self.gen_umsatz_prognose(pandas_sweeped_dt)
-                    # calculate the individual errors
-                    umsatz_potential_pd['E_i'] = np.power(umsatz_potential_pd['Umsatz_Haushalte'] -
-                                                          umsatz_potential_pd['istUmsatz'], 2) / \
-                        umsatz_potential_pd['istUmsatz']
-
-                    total_error = np.sqrt(umsatz_potential_pd.E_i.sum())
-                    error_quantile = umsatz_potential_pd[~np.isnan(umsatz_potential_pd['E_i'])]['E_i'].quantile(q=0.99)
-                    total_error_0_99 = np.sqrt(umsatz_potential_pd[umsatz_potential_pd['E_i'] <=
-                                                                   error_quantile]['E_i'].sum())
-
-                    self.logger.info("TOTAL ERROR: %f / %f", total_error, total_error_0_99)
-
-                    if total_error_0_99 < self.E_min[0][0]:
-                        self.E_min = [(total_error, {"lat": lat, "rlat": rlat, "fz_cutoff": fz})]
-                        self.logger.info('New minimum found.')
-
-                    self.logger.info('Exporting Umsatz predictions to csv')
-
-                    umsatz_potential_pd.to_csv(self.umsatz_output_csv + '_lat_' + str(lat) + '_rlat_' + str(rlat) +
-                                               '_fz_' + str(fz))
-
-        self.logger.info('Found error minimum of %f for lat=%f / rlat=%f / fz_cutoff=%f',
-                         self.E_min[0][0], self.E_min[0][1]["lat"], self.E_min[0][1]["rlat"],
-                         self.E_min[0][1]["fz_cutoff"])
 
     def gen_umsatz_prognose(self, pandas_pd):
         self.logger.info('Generating Umsatz predictions ... ')
